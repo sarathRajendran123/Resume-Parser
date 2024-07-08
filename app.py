458fff1cafa2fe397cef06fca521c95e54603ade
Sarath Rajendran <sarathrp2003@gmail.com>
2024-07-08 11:09:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def input_pdf_setup(uploaded_file):
 ## Streamlit App
 
 st.set_page_config(page_title="ATS Resume Expert")
-st.header("ATS Tracking System")
+st.header("Resume Insight")
 input_text=st.text_area("Job Description: ",key="input")
 uploaded_file=st.file_uploader("Upload your resume in pdf",type=["pdf"])
 
@@ -56,6 +56,8 @@ submit2 = st.button("How Can I Improvise my Skills")
 
 submit3 = st.button("Percentage match")
 
+submit4 = st.button("Significant Accomplishments in the resume")
+
 
 
 input_prompt1 = """
@@ -64,11 +66,23 @@ input_prompt1 = """
  Highlight the strengths and weaknesses of the applicant in relation to the specified job requirements.
 """
 
+input_prompt2= """
+You are an experienced Career Development Coach. Your task is to provide a comprehensive plan for skill improvement tailored to the 
+individual seeking guidance. Please share your professional advice on how they can enhance their skills effectively.
+"""
+
 input_prompt3 = """
 You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
 the job description. First the output should come as percentage and then keywords missing and last final thoughts.
 """
+input_prompt4 = """
+You are reviewing a candidate's resume and need to identify significant accomplishments. Please highlight the most impactful achievements 
+listed in the resume. Consider quantifiable achievements, such as specific projects completed, awards received, metrics improved, or 
+challenges overcome. Focus on identifying accomplishments that demonstrate the candidate's skills, contributions, and potential value to a 
+prospective employer.
+"""
+
 
 if submit1:
     if uploaded_file is not None:
@@ -82,7 +96,7 @@ if submit1:
 elif submit2:
     if uploaded_file is not None:
         pdf_content=input_pdf_setup(uploaded_file)
-        response=get_gemini_response(input_prompt1,pdf_content,input_text)
+        response=get_gemini_response(input_prompt2,pdf_content,input_text)
         st.subheader("The Repsonse is")
         st.write(response)
     else:
@@ -96,6 +110,17 @@ elif submit3:
         st.write(response)
     else:
         st.write("Please upload the resume")
+        
+        
+elif submit4:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt4,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please upload the resume")
+
 
 
 
